19bc9e88566e3deb8249d1e2c46d9ff050762140
idktigerz <idktigerzz@gmail.com>
2023-01-14 23:34:43 +0000

    a

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+
+require('dotenv').config();
 
 const { Client } = require('pg');
 const client = new Client({
