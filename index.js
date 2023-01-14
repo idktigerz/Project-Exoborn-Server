0423515c3558aa481b54cd78d91f51bb66c7c0da
idktigerz <idktigerzz@gmail.com>
2023-01-14 22:32:15 +0000

    a

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// Use body-parser middleware to parse incoming JSON data
+app.use(bodyParser.json());
+
+// Define a GET route for the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Define a POST route to handle a user login
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    if (username === 'admin' && password === 'password') {
+        res.send('Welcome, Admin!');
+    } else {
+        res.send('Invalid username or password');
+    }
+});
+
+// Start the server on port 3000
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
