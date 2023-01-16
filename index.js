e67d70eb9dfc05c434d84fd69a5bd89e560e4314
idktigerz <idktigerzz@gmail.com>
2023-01-16 17:27:11 +0000

    a

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+var pg = require('pg');
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,6 +9,16 @@ const bodyParser = require('body-parser');
 var droneRouter = require('./routes/droneRoutes');
 var gameRouter = require('./routes/gameRoutes');
 
+const connectionString = process.env.DATABASE_URL
+const Pool = pg.Pool
+const pool = new Pool({
+    connectionString,
+    ssl: {
+        require: true, 
+        rejectUnauthorized: false
+    },
+    max: 1
+})
 
 // Use body-parser middleware to parse incoming JSON data
 app.use(bodyParser.json());
@@ -17,6 +29,18 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/game', gameRouter);
+
+app.get('/get/game', (req, res) =>{
+    pool.query('SELECT * from game_connection', (err, result) =>{
+        if(err){
+            res.status(500).json({
+                error:err.message
+            });
+        }else{
+            res.json(result.rows);
+        }
+    });
+});
 
 
 // Start the server on port 3000
