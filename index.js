7d1e99a139b40926f27cd4b1ba855ddb83ce306c
idktigerz <idktigerzz@gmail.com>
2023-01-17 22:28:00 +0000

    a

--- index.js
+++ index.js
@@ -82,6 +82,18 @@ app.get('/get/player', (req, res) =>{
     })
 })
 
+
+app.get('/get/drone', (req, res) =>{
+    pool.query('SELECT * from drone', (err, result) => {
+        if(err){
+            res.status(500).json({
+                error:err.message
+            });
+        }else{
+            res.json(result.rows);
+        }
+    })
+})
 // Start the server on port 3000
 app.listen(3000, () => {
     console.log('Server started on port 3000');
