69d86fd05bbab6c39c4f33e81f0d634f2e5f24d9
idktigerz <idktigerzz@gmail.com>
2023-01-18 12:35:49 +0000

    a

--- index.js
+++ index.js
@@ -108,9 +108,9 @@ app.get('/get/drone/:id', (req, res) =>{
     })
 })
 
-app.put('/set/drone/:id/:upgradeNum', (req, res) =>{
+app.put('/set/drone/:id/', (req, res) =>{
     const id = req.params.id;
-    const upgradeNum = req.params.upgradeNum;
+    const upgradeNum = req.body.upgradeNum;
     const upgradeNumID = req.body.upgradeNumID;
     console.log("ID: " + id);
     console.log("Upgrade Num: " + upgradeNum);
