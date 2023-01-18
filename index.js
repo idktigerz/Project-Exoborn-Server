de23bef1fbaf342923e6ee8a007d57d75621b7c4
idktigerz <idktigerzz@gmail.com>
2023-01-18 12:18:51 +0000

    a

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.put('/set/drone/:id/:upgradeNum', (req, res) =>{
                 error:err.message
             });
         }else{
-            res.json(res.rows);
+            res.send(res.rows);
         } 
     });
 });
